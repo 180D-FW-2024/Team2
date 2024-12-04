e24aba86f1898f94e5818f78bc6ec54ed0a028e4
lawalnut <lauren042503@gmail.com>
2024-12-04 09:31:49 -0800

    Update motors.py

--- motors.py
+++ motors.py
@@ -1,3 +1,5 @@
+# Raspberry Pi Pico
+
 from machine import Pin
 import time
 
@@ -15,25 +17,25 @@ def stop_all():
         motor.off() 
 
 # Directions
-def stop(duration = 0.5):
+def stop(duration):
     for motor in [M1, M2, M3, M4, M5, M6]: 
         motor.on()  
         time.sleep(duration) 
         motor.off()
 
-def move_forward(duration = 0.5):
+def move_forward(duration):
     M3.on()
     time.sleep(duration)
     M3.off()
 
-def turn_left(duration = 0.5):
+def turn_left(duration):
     M1.on()  
     M2.on()  
     time.sleep(duration)
     M1.off()
     M2.off()
 
-def turn_right(duration = 0.5):
+def turn_right(duration):
     M4.on()  
     M5.on()  
     time.sleep(duration)
